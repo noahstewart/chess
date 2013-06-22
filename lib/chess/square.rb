43c01bbe3455153d7754e0108bcdeb4488541bc8
Zac Stewart <zgstewart@gmail.com>
2013-06-22 02:35:53 -0400

    Create dark and light squares

--- lib/chess/square.rb
+++ lib/chess/square.rb
@@ -1,7 +1,21 @@
 module Chess
   class Square
+
+    def initialize(color)
+      @color = color
+    end
+
     def occupied?
       false
     end
+
+    def light?
+      @color == :light
+    end
+
+    def dark?
+      @color == :dark
+    end
+
   end
 end
--- spec/square_spec.rb
+++ spec/square_spec.rb
@@ -1,5 +1,23 @@
 require_relative '../lib/chess/square'
 
 describe Chess::Square do
+  subject { Chess::Square.new(:dark) }
+
   it { should_not be_occupied }
+
+  describe 'dark square' do
+    subject { Chess::Square.new(:dark) }
+
+    it { should_not be_light }
+
+    it { should be_dark }
+  end
+
+  describe 'light square' do
+    subject { Chess::Square.new(:light) }
+
+    it { should be_light }
+
+    it { should_not be_dark }
+  end
 end
